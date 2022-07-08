f47e40e1ee52aab758b717db443a295df5d4f6f2
daniellicodes <777danielli777@gmail.com>
2022-07-08 15:48:49 -0700

    added tab to display c code

---
+++ web/src/components/tabs/CCode.vue
@@ -0,0 +1,66 @@
+<template>
+    <code-highlight language="cpp">
+            using namespace std;
+
+            int main(){
+                int n;
+                cin >> n;
+                while(n--){
+                    int days, hours, minutesWorked;
+                    cin >> days >> hours;
+                    hours*=60;
+                    minutesWorked=0;
+                    while(days--){
+                        int startMinutes, endMinutes;
+                        int temp;
+                        char zzz;
+                        cin >> startMinutes;
+                        startMinutes*=60;
+                        cin >> zzz >> temp;
+                        startMinutes+=temp;
+                        cin >> endMinutes;
+                        endMinutes*=60;
+                        cin >> zzz >> temp;
+                        endMinutes+=temp;
+                        minutesWorked+=(endMinutes-startMinutes);
+                    }
+                    if(minutesWorked >= hours){
+                        cout << "YES" << endl;
+                    }else{
+                        cout << "NO" << endl;
+                    }
+                    
+                }
+            }
+    </code-highlight>
+
+</template>
+<script> 
+import CodeHighlight from "vue-code-highlight/src/CodeHighlight.vue";
+import "vue-code-highlight/themes/duotone-sea.css";
+
+export default {
+  name: 'CCode',
+  components:{
+      CodeHighlight
+  },
+//   props:{
+      
+//   },
+//   data(){
+//   },
+//   mounted() {
+//         //add code here
+//   },
+//   methods: {
+    
+//   }
+}
+</script>
+<style>
+    text {
+        font-weight: 300;
+        font-family: "Helvetica Neue", Helvetica, Arial, sans-serf;
+        font-size: 14px;
+    }
+</style>
