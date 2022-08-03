ccd0e346e981f922b2f31b405e907edb1b2f18c9
daniellicodes <777danielli777@gmail.com>
2022-08-02 19:21:38 -0700

    changed theme, lint

--- web/src/components/tabs/CCode.vue
+++ web/src/components/tabs/CCode.vue
@@ -1,41 +1,36 @@
-
 <template>
-    <div>
+    <div ref = "codeBlock">
         <code-highlight language="c">
-            {{cCode==undefined || cCode == '' ? dataToDisplay:cCode}}
+            <!-- <pre :class="languageClass"><code><slot /></code></pre> -->
+            <pre>{{(cCode == undefined || cCode == '' ) ? dataToDisplay:cCode}}</pre>
         </code-highlight>
     </div>
 </template>
 <script> 
 import CodeHighlight from "vue-code-highlight/src/CodeHighlight.vue";
-import "vue-code-highlight/themes/duotone-sea.css";
+import "vue-code-highlight/themes/prism-solarizedlight.css";
 import { mapState } from 'vuex'
-// import Prism from 'prismjs'
+import Prism from 'prismjs'
 import 'prismjs/components/prism-c'
 export default {
   data: function(){
-    return {dataToDisplay:"C Code from retdec here!\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n", shortname:""}
+    return {dataToDisplay:"//C Code from retdec here!\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n", shortname:""}
   },
   name: 'CCode',
   components:{
       CodeHighlight
   },
-  computed:mapState(['cCode']),
-  mounted: {
+  computed: mapState(['cCode']),
+  mounted () {
+    Prism.highlightAllUnder(this.$refs.codeBlock)
+  },
+  beforeUpdate () {
+    this.$el.querySelector('code').textContent = this.$store.state.cCode.replace(/^[\r\n\s]*|[\r\n\s]*$/g, '')
+    Prism.highlightAllUnder(this.$refs.codeBlock)
   },
   methods: {
-  }, 
-//   props:{
-      
-//   },
-//   data(){
-//   },
-//   mounted() {
-//         //add code here
-//   },
-//   methods: {
-    
-//   }
+  }
+
 }
 </script>
 <style>
